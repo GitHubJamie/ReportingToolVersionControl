d45d593d239cec355f3c21c7d8131125fdab2852
jamie <jamie@tectumai.com>
2017-08-11 11:48:18 +0100

    Commit 2

--- WebReportingTool/WebReportingTool/WebForm1.aspx.cs
+++ WebReportingTool/WebReportingTool/WebForm1.aspx.cs
@@ -16,7 +16,7 @@ namespace WebReportingTool
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            TextBox1.Text = TextBox1.Text + "hello ";
+            TextBox1.Text = TextBox1.Text + " hello ";
         }
     }
 }
